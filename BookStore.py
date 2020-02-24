5250c67df4fb23a201deec67b78e0c13eea78365
dbe2843 <60494698+dbe2843@users.noreply.github.com>
2020-02-25 09:22:33 +1300

    Bookstore Add

--- BookStore.py
+++ BookStore.py
@@ -9,6 +9,13 @@ print("Welcome to the bookstore! Please type one of the following commands below
 for index in range(0, len(commands)):
     print(commands[index])
 run_command = input()
-
+'''Running the add command'''
+if run_command == "add":
+    name = input("Enter book name")
+    author = input("Enter an author")
+    price = int(input("Enter a price"))
+    books.append([name, author, price])
+print(books)
+'''Running the edit command'''
     
     
