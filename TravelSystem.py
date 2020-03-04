de6f416a40798794316bfa0c82ee8970a396dde3
dbe2843 <60494698+dbe2843@users.noreply.github.com>
2020-03-05 09:20:32 +1300

    TravelSystem
    Removed brackets from list and added numbers

--- TravelSystem.py
+++ TravelSystem.py
@@ -1,22 +1,22 @@
 # Travel System task
 # Lists for the system
-commands = [["Plan Trip: plan"], ["Quit program: quit"]]
 departure_locations = [["Christchurch"], ["Wellington"], ["Auckland"]]
 overseas_locations = [""]
 
 # Welcoming the user to the Travel System
 
 print ("Hello, this programme allows you to calculate the cost of traveling between places and the cost of accomodation")
-print ("Please enter one of the following commands:")
-for index in range(0, len(commands)):
-    print(commands[index])
-run_command = input()
+print ('''Please enter one of the following commands:
+   1) Plan a flight
+   2) Quit program''')
+run_command = int(input())
 # Running the plan command
-if run_command == "plan":
+if run_command == 1:
+    # Selecting a departure location
     print ("Select a departure location:")
     for index in range(0, len(departure_locations)):
-        print(departure_locations[index])
-    run_command = input()    
+        print(index+1, departure_locations[index][0])  
     departure = input()
+    # Select a destination
     print ("Select a destination:")
     destination = input()
